f9fa5ee1aec26d6176d017d9a0bd1d0e7b62cf63
Ubuntu <ubuntu@ip-172-31-3-237.ap-south-1.compute.internal>
2026-02-06 05:13:20 +0000

    server changes

--- rk_certificate_portal/settings.py
+++ rk_certificate_portal/settings.py
@@ -15,17 +15,24 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY","change-this")
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['sruniq.com']
 
 INSTALLED_APPS = [
- 'django.contrib.admin','django.contrib.auth','django.contrib.contenttypes',
- 'django.contrib.sessions','django.contrib.messages','django.contrib.staticfiles',
- 'storages','certificates'
+ 'django.contrib.admin',
+ 'django.contrib.auth',
+ 'django.contrib.contenttypes',
+ 'django.contrib.sessions',
+ 'django.contrib.messages',
+ 'django.contrib.staticfiles',
+ 'storages',
+ 'certificates',
+ "corsheaders",
 ]
 
 MIDDLEWARE = [
  'django.middleware.security.SecurityMiddleware',
  'django.contrib.sessions.middleware.SessionMiddleware',
+ "corsheaders.middleware.CorsMiddleware",
  'django.middleware.common.CommonMiddleware',
  'django.middleware.csrf.CsrfViewMiddleware',
  'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -57,8 +64,8 @@ DATABASES = {
 LANGUAGE_CODE='en-us'
 TIME_ZONE='UTC'
 USE_TZ=True
-# STATIC_URL='/static/'
-# STATIC_ROOT=BASE_DIR/'staticfiles'
+STATIC_URL='/static/'
+STATIC_ROOT=BASE_DIR/'staticfiles'
 
 
 # settings.py
@@ -76,17 +83,17 @@ DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID=os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY=os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME=os.environ.get("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_REGION_NAME='ap-south-1'
+AWS_S3_REGION_NAME=os.environ.get("AWS_S3_BUCKET_REGION")
 # AWS_DEFAULT_ACL = None
 AWS_S3_FILE_OVERWRITE = False
 AWS_QUERYSTRING_AUTH = True
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-AWS_S3_BUCKET_REGION = env('AWS_S3_BUCKET_REGION')
+AWS_S3_BUCKET_REGION =os.environ.get("AWS_S3_BUCKET_REGION")
 # s3 static settings
-STATIC_LOCATION = 'static'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
-STATICFILES_STORAGE = 'rk_certificate_portal.storage_backend.StaticStorage'
+#STATIC_LOCATION = 'static'
+#STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+#STATICFILES_STORAGE = 'rk_certificate_portal.storage_backend.StaticStorage'
 # s3 public media settings
 PUBLIC_MEDIA_LOCATION = 'media'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
